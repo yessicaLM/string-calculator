6f08132994383915ff93d2d3d9a9ed2404fb253a
yessica <yessica@leanmind.es>
2022-07-04 08:19:14 +0100

    GREEN: given string '2,2' returns 4

--- src/core/stringCalculator.ts
+++ src/core/stringCalculator.ts
@@ -1,4 +1,11 @@
 export const stringCalculator = (numbers: string | number): number => {
+  if (typeof numbers === 'string') {
+    const stringNumbers = String(numbers)
+    if (stringNumbers.includes(',')) {
+      const listOfNumers = stringNumbers.split(',');
+      return listOfNumers.reduce((numA, numB) => Number(numA) + Number(numB), 0)
+    }
+  }
   if(numbers === '2') return 2;
   return 0;
 }
