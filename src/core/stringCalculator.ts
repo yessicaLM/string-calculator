a24e6dbe0530d5323133a1585685aa9b7cf19157
yessica <yessica@leanmind.es>
2022-07-04 07:53:36 +0100

    GREEN: empty string returns 0

---
+++ src/core/stringCalculator.ts
@@ -0,0 +1,3 @@
+export const stringCalculator = (numbers: string | number): number => {
+  return 0;
+}
--- src/tests/stringCalculator.test.ts
+++ src/tests/stringCalculator.test.ts
@@ -1,9 +1,13 @@
-// null or '' -> 0
+import { stringCalculator } from '../core/stringCalculator';
+
+// null -> 0 --> DONE
+// '' -> 0
 // '2' -> 2
 // 2,2 -> 4
 // 2,2,a -> 4
 // n,n -> 0
 // //%/2%2 -> 4
+
 
 describe('String calculator', () => {
   it('empty string returns 0', () => {
