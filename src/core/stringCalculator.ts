c331f84d601dd95599bb475dae49a854e4da495e
yessica <yessica@leanmind.es>
2022-07-04 08:56:07 +0100

    REFACTOR: string calculator file

--- src/core/stringCalculator.ts
+++ src/core/stringCalculator.ts
@@ -1,11 +1,19 @@
+const isStringValue = (value: string | number): boolean => typeof value === 'string';
+
+const listSeparator = (strinOfNumbers: string): string => strinOfNumbers.includes(',') 
+  ? ',' 
+  : strinOfNumbers[2];
+
+const sumValues = (numbers: string): number => {
+  const listOfNumbers = numbers.split(listSeparator(numbers));
+  const filteredNumbers = listOfNumbers.map(number => number.replace(/[^\d.-]/g, ''));
+  return filteredNumbers.reduce((numA, numB) => Number(numA) + Number(numB), 0);
+}
+
+
 export const stringCalculator = (numbers: string | number): number => {
-  if (typeof numbers === 'string') {
-    const stringNumbers = String(numbers);
-    const separator = stringNumbers.includes(',') ? ',' : stringNumbers[2];
-    const listOfNumers = stringNumbers.split(separator);
-    const filteredNumbers = listOfNumers.map(number => number.replace(/[^\d.-]/g, ''));
-    return filteredNumbers.reduce((numA, numB) => Number(numA) + Number(numB), 0);
+  if (isStringValue(numbers)) {
+    return sumValues(String(numbers));    
   }
   if (!numbers) return 0;
-  if (typeof numbers === 'string') return Number(numbers);
 }
