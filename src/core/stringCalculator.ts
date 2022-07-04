87fc397c72b554424a3d008e585df11ebfa822fa
yessica <yessica@leanmind.es>
2022-07-04 08:44:28 +0100

    REFACTOR: string calculator file

--- src/core/stringCalculator.ts
+++ src/core/stringCalculator.ts
@@ -13,6 +13,6 @@ export const stringCalculator = (numbers: string | number): number => {
       return filteredNumbers.reduce((numA, numB) => Number(numA) + Number(numB), 0);
     }
   }
-  if(numbers === '2') return 2;
-  return 0;
+  if (!numbers) return 0;
+  if(typeof numbers === 'string') return Number(numbers);
 }
