e86564f9568cf797a996919bd8102cb529896714
yessica <yessica@leanmind.es>
2022-07-04 08:37:20 +0100

    GREEN: accepts customized sequences: '//%/2%2' returns 4

--- src/core/stringCalculator.ts
+++ src/core/stringCalculator.ts
@@ -6,6 +6,12 @@ export const stringCalculator = (numbers: string | number): number => {
       const filteredNumbers = listOfNumers.map(number => number.replace(/[^\d.-]/g, ''));
       return filteredNumbers.reduce((numA, numB) => Number(numA) + Number(numB), 0);
     }
+    if(stringNumbers.startsWith('//')) {
+      const separator = stringNumbers[2];
+      const listOfNumers = stringNumbers.split(separator);
+      const filteredNumbers = listOfNumers.map(number => number.replace(/[^\d.-]/g, ''));
+      return filteredNumbers.reduce((numA, numB) => Number(numA) + Number(numB), 0);
+    }
   }
   if(numbers === '2') return 2;
   return 0;
