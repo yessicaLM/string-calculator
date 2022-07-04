7e91685ac5966f214eda0be9ad7ee2b2f887d0e2
yessica <yessica@leanmind.es>
2022-07-04 08:49:20 +0100

    REFACTOR: string calculator file

--- src/core/stringCalculator.ts
+++ src/core/stringCalculator.ts
@@ -1,18 +1,11 @@
 export const stringCalculator = (numbers: string | number): number => {
   if (typeof numbers === 'string') {
-    const stringNumbers = String(numbers)
-    if (stringNumbers.includes(',')) {
-      const listOfNumers = stringNumbers.split(',');
-      const filteredNumbers = listOfNumers.map(number => number.replace(/[^\d.-]/g, ''));
-      return filteredNumbers.reduce((numA, numB) => Number(numA) + Number(numB), 0);
-    }
-    if(stringNumbers.startsWith('//')) {
-      const separator = stringNumbers[2];
-      const listOfNumers = stringNumbers.split(separator);
-      const filteredNumbers = listOfNumers.map(number => number.replace(/[^\d.-]/g, ''));
-      return filteredNumbers.reduce((numA, numB) => Number(numA) + Number(numB), 0);
-    }
+    const stringNumbers = String(numbers);
+    const separator = stringNumbers.includes(',') ? ',' : stringNumbers[2];
+    const listOfNumers = stringNumbers.split(separator);
+    const filteredNumbers = listOfNumers.map(number => number.replace(/[^\d.-]/g, ''));
+    return filteredNumbers.reduce((numA, numB) => Number(numA) + Number(numB), 0);
   }
   if (!numbers) return 0;
-  if(typeof numbers === 'string') return Number(numbers);
+  if (typeof numbers === 'string') return Number(numbers);
 }
