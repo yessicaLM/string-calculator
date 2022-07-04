c22cd4cbc9588ec337110433385cb3fef5257239
yessica <yessica@leanmind.es>
2022-07-04 08:00:15 +0100

    RED: given string '2,2' returns 4

--- src/tests/stringCalculator.test.ts
+++ src/tests/stringCalculator.test.ts
@@ -2,7 +2,7 @@ import { stringCalculator } from '../core/stringCalculator';
 
 // null -> 0 --> DONE
 // '' -> 0 --> DONE
-// '2' -> 2
+// '2' -> 2 --> DONE
 // 2,2 -> 4
 // 2,2,a -> 4
 // n,n -> 0
@@ -20,5 +20,9 @@ describe('String calculator', () => {
 
   it('given string of 2 returns number 2', () => {
     expect(stringCalculator('2')).toBe(2);
+  });
+
+  it('given string "2,2" returns 4', () => {
+    expect(stringCalculator('2,2')).toBe(4);
   });
 });
